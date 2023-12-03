3a47d6aba6824484127ecbf0b4987b76775706b5
itlackey <itlackey@gmail.com>
2023-12-03 00:04:28 -0600

    added example log

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,6 @@
 
 # https://github.com/ggerganov/llama.cpp/blob/master/examples/server/README.md
-MODEL_NAME=wizardlm-1.0-uncensored-llama2-13b.Q5_K_M.gguf
+#MODEL_NAME=wizardlm-1.0-uncensored-llama2-13b.Q5_K_M.gguf
 MODEL_NAME=llama-2-7b-chat.Q4_0.gguf
 
-./build/bin/server -m /models/$MODEL_NAME -c 2048 --port 3000 -ngl 41  --host 0.0.0.0
+./build/bin/server -m /models/$MODEL_NAME -c 2048 --port 3000 -ngl 33  --host 0.0.0.0
